4933a1c78ceaf77f82b65b22f7d2bafc3dda1d82
Guilherme Rossato <guilherme@fastcompras.com>
2017-09-14 14:15:45 -0300

    Added function commentary and general error checking
    For basic functionality test

--- app/code/local/OrderFilter/Model/Observer.php
+++ app/code/local/OrderFilter/Model/Observer.php
@@ -2,6 +2,12 @@
 
 class GuilhermeRossato_OrderFilter_Model_Observer
 {
+    /*
+    * This functions parses an order and cancels it if anything doesn't match a hardcoded criteria.
+    *
+    * To cancel a order request, use raiseError($message) method:
+    *     throw Mage::throwException("Order blocked due to address not being whitelisted");
+    */
     public function filterOrder($event)
     {
     }
@@ -11,6 +17,10 @@ class GuilhermeRossato_OrderFilter_Model_Observer
         Mage::getSingleton('core/session')->addNotice($errMessage);
         throw Mage::throwException("Order blocked due to address not being whitelisted");
     }
+
+    /*
+    * For when an order has to be modified / something has to be done
+    */
     private function _processOrderStatus($order)
     {
         $invoice = $order->prepareInvoice();
@@ -23,10 +33,22 @@ class GuilhermeRossato_OrderFilter_Model_Observer
         $invoice->sendEmail(true, '');
         return true;
     }
+
+    /*
+    * The following functions happens before the "filterObject" method. Both methods happen before the order is placed.
+    * Depending on how the payment is done, this might land you in a different page entirely
+    */
     public function checkBeforeSaving(Varien_Event_Observer $observer) {
+        $event = $observer->getEvent();
+        $order = $event->getOrder();
+        $configForTest = Mage::getStoreConfig('guilhermerossato/guilhermerossato_group/guilhermerossato_input',Mage::app()->getStore());
+        $this->raiseError("Blocked before everything - before");
         return $this;
     }
     public function checkAfterSaving(Varien_Event_Observer $observer) {
+        $event = $observer->getEvent();
+        $order = $event->getOrder();
+        $this->raiseError("Blocked before everything - after");
         return $this;
     }
 }
